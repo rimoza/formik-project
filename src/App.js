1bce11102fdee2a7a0ec221d57b5533d5a5d0080
Ridwan Mohamed <ridwan.maxamed1@outlook.com>
2022-08-22 18:13:52 +0300

    Feat: Add form elements

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import {Formik, Form, Field} from 'formik';
+import {Container, Card, CardContent, Typography, Button, Grid, TextField} from '@mui/material';
 function App() {
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container sx={{ bgcolor: "#87c1ff4d", paddingY: 3, marginTop: 5 }}>
+    <Typography variant="h3" align="center" component="h2">
+      Formik Form
+    </Typography>
+    <Card sx={{ marginTop: 2 }}>
+      <CardContent sx={{ paddingY: 10, paddingX: 5 }}>
+        <Formik>
+          <Form>
+            <Grid container spacing={2}>
+              <Grid item md={6}>
+                <Field fullWidth name="firstname" component={TextField} label="First Name" />
+              </Grid>
+              <Grid item md={6}>
+                <Field fullWidth name="lastname" component={TextField} label="Last Name" />
+              </Grid>
+            </Grid>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <Typography variant="h6" component="h2">
+                  Add Social Links
+                </Typography>
+              </Grid>
+                <Grid item md={5}>
+                  <Field fullWidth name="linkname" component={TextField} label="Link Name" />
+                </Grid>
+                <Grid item md={5}>
+                  <Field fullWidth name="linkurl" component={TextField} label="Link URL" />
+                </Grid>
+                <Grid item md={2}>
+                  <Button variant="outlined" color="error">
+                    Delete
+                  </Button>
+                </Grid>
+              <Grid item>
+                <Button variant="outlined" >Add Link</Button>
+              </Grid>
+            </Grid>
+          </Form>
+        </Formik>
+      </CardContent>
+    </Card>
+  </Container>
   );
 }
 
